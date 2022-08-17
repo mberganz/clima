fe3ced140ada504168d5f929d9155b4c358ee578
mberganz <108034006+mberganz@users.noreply.github.com>
2022-08-16 21:52:19 -0300

    script final

--- clima/js/script.js
+++ clima/js/script.js
@@ -2,14 +2,14 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
   event.preventDefault();
   clearInfo();
 
-  let input = document.querySelector("#searchInput").value();
+  let input = document.querySelector("#searchInput").value;
 
   if (input != "") {
     showWarning("Carregando...");
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?=${encodeURI(
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
       input
-    )}&appid=bd9d8015509aaa28777d5541cc90a35c&units=metric&lang=pt_br`;
+    )}&appid=cbd395dee0b5876200c4dbe7233a54ff&units=metric&lang=pt_br`;
 
     let results = await fetch(url);
     let json = await results.json();
@@ -18,10 +18,11 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
       showInfo({
         name: json.name,
         country: json.sys.country,
+        temp: json.main.temp,
         climaIcon: json.weather[0].icon,
         climaTexto: json.weather[0].description,
         windSpeed: json.wind.speed,
-        windAngle: json.wind.degree,
+        windAngle: json.wind.deg,
       });
     } else {
       showWarning("Não encontramos esta localização!");
@@ -29,7 +30,7 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
   }
 });
 
-function showWarning() {
+function showWarning(msg) {
   document.querySelector(".aviso").innerHTML = msg;
 }
 
@@ -46,4 +47,19 @@ function showInfo(dados) {
     ".titulo"
   ).innerHTML = `${dados.name}, ${dados.country}`;
   document.querySelector(".tempInfo").innerHTML = `${dados.temp} <sup>°C</sup>`;
+  document.querySelector(
+    ".ventoInfo"
+  ).innerHTML = `${dados.windSpeed} <sup>Km/h</sup>`;
+  document
+    .querySelector(".temp img")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${dados.climaIcon}@2x.png`
+    );
+  document.querySelector(".climaTitulo").innerHTML = `${dados.climaTexto}`;
+  document.querySelector(".ventoPonto").styleTransform = `rotate(${
+    dados.windAngle - 90
+  }deg)`;
+
+  document.querySelector(".resultado").style.display = "block";
 }
